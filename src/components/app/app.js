0db791abe298b15f8261d0d5d3b76b9a5c765a22
Valli <nekotyantv@gmail.com>
2022-09-27 22:05:53 +0300

    optimization of the method of adding and removing employees and Lifting State Up

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,28 +8,86 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import "./app.css";
 
-function App() {
-  const data = [
-    { name: "OlegGet", salary: 800, increase: true, id: 1 },
-    { name: "morskoyzmei", salary: 5000, increase: false, id: 2 },
-    { name: "Olya", salary: 3500, increase: false, id: 3 },
-    { name: "Demetris", salary: 580, increase: false, id: 4 },
-    { name: "Lex", salary: 999, increase: false, id: 5 },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "OlegGet", salary: 800, increase: true, rise: false, id: 1 },
+        {
+          name: "morskoyzmei",
+          salary: 5000,
+          increase: false,
+          rise: false,
+          id: 2,
+        },
+        { name: "Olya", salary: 3500, increase: false, rise: false, id: 3 },
+        { name: "Demetris", salary: 580, increase: false, rise: true, id: 4 },
+        { name: "Lex", salary: 999, increase: false, rise: false, id: 5 },
+      ],
+    };
+    this.maxId = 6;
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  //удаление сотрудника
+  deleleItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  // добавление сотрудника
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      rise: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArray = [...data, newItem];
+      return {
+        data: newArray,
+      };
+    });
+  };
+
+  // метод изменения параментров инкрис и повышения на противоположный
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, [prop]: !item[prop] };
+        }
+        return item;
+      }),
+    }));
+  };
+
+  render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
+    return (
+      <div className="app">
+        <AppInfo employees={employees} increased={increased} />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleleItem}
+          onToggleProp={this.onToggleProp}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -12,9 +12,20 @@ class EmployeesAddForm extends Component {
 
   onValueChange = (e) => {
     this.setState({
-      [e.target.name]: [e.target.value],
+      [e.target.name]: e.target.value
     });
   };
+
+
+  // метод добавления нового сотрудника в список
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.state.name, this.state.salary);
+    this.setState({
+      name: '',
+      salary: ''
+    })
+  }
 
   render() {
     const { name, salary } = this.state;
@@ -22,7 +33,8 @@ class EmployeesAddForm extends Component {
     return (
       <div className="app-add-form">
         <h3>Добавьте нового сотрудника</h3>
-        <form className="add-form d-flex">
+        <form className="add-form d-flex"
+              onSubmit={this.onSubmit}>
           <input
             type="text"
             className="form-control new-post-label"
@@ -40,7 +52,8 @@ class EmployeesAddForm extends Component {
             onChange={this.onValueChange}
           />
 
-          <button type="submit" className="btn btn-outline-light">
+          <button type="submit"
+                  className="btn btn-outline-light">
             Добавить
           </button>
         </form>
